e8a29fcb21f19e31227323f903a76b3550f28437
akash halder <akashhalder277@gmail.com>
2025-04-14 21:53:01 +0530

    configure global.css, babel, metro, nativewind-env, tailwind-config, include tailwind css into layout.

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 justify-between items-center"
     >
-      <Text>welcome</Text>
+      <Text className="text-5xl text-dark-200 font-bold">welcome</Text>
     </View>
   );
 }
