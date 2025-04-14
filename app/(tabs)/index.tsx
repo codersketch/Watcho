2a0153e24d678bbe5a690c5183b9e756dc5936ca
akash halder <akashhalder277@gmail.com>
2025-04-15 01:40:32 +0530

    added routes and tab bar, with some assets.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      className="flex-1 justify-between items-center"
+      className="flex justify-between items-center"
     >
       <Text className="text-5xl text-dark-200 font-bold">welcome</Text>
     </View>
