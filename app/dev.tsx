768cccd36b448dcfee78d9cc519bb5b8bad95c79
akash halder <akashhalder277@gmail.com>
2025-04-18 23:24:44 +0530

    Created search bar and api cervices form tmdb api service.

---
+++ app/dev.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const dev = () => {
+    return (
+        <View>
+            <Text>dev</Text>
+        </View>
+    );
+};
+
+export default dev;
+
+const styles = StyleSheet.create({});
